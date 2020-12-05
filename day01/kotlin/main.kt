b95e6e13eabc816bc55cd8f270bfd7fd4424b852
Simon Duchastel <simondmr@cs.washington.edu>
2020-12-05 14:18:31 -0800

    day01 part2: too complicated with enum

--- day01/kotlin/main.kt
+++ day01/kotlin/main.kt
@@ -2,6 +2,13 @@ import kotlin.system.exitProcess
 import kotlin.io.readLine
 import java.io.File
 
+enum class CompilationMode {
+    FIND_PAIR,
+    FIND_TRIPLE
+}
+
+val COMPILATION_MODE = CompilationMode.FIND_TRIPLE
+
 /**
  *  Returns two integers in the list which sum to 2020, or null
  *  if none are found
@@ -11,6 +18,23 @@ fun findIntegerPair(ints: List<Int>): Pair<Int, Int>? {
         for (j in (i+1) until ints.size) {
             if (ints[i] + ints[j] == 2020) {
                 return Pair(ints[i], ints[j])
+            }
+        }
+    }
+    return null
+}
+
+/**
+ *  Returns three integers in the list which sum to 2020, or null
+ *  if none are found
+ */
+fun findIntegerTriple(ints: List<Int>): Triple<Int, Int, Int>? {
+    for (i in 0 until ints.size) {
+        for (j in (i+1) until ints.size) {
+            for (k in (j+1) until ints.size) {
+                if (ints[i] + ints[j] + ints[k] == 2020) {
+                    return Triple(ints[i], ints[j], ints[k])
+                }
             }
         }
     }
@@ -30,8 +54,16 @@ fun main(args : Array<String>) {
         exitWithMessage("Error parsing input: non-integer found")
     }
 
-    val (firstInt, secondInt) = findIntegerPair(parsedInts) ?: exitWithMessage("Unable to find answer")
-    println("The product of $firstInt and $secondInt is ${firstInt * secondInt}")
+    when (COMPILATION_MODE) {
+        CompilationMode.FIND_PAIR -> {
+            val (firstInt, secondInt) = findIntegerPair(parsedInts) ?: exitWithMessage("Unable to find answer")
+            println("The product of $firstInt and $secondInt is ${firstInt * secondInt}")
+        }
+        CompilationMode.FIND_TRIPLE -> {
+            val (firstInt, secondInt, thirdInt) = findIntegerTriple(parsedInts) ?: exitWithMessage("Unable to find answer")
+            println("The product of $firstInt, $secondInt, and $thirdInt is ${firstInt * secondInt * thirdInt}")
+        }
+    }
 }
 
 fun readIntegers(getNextLine: () -> String?): List<Int> {
